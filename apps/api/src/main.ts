6a9a0ee35de8319539630817fcbcd3f65f220415
Don Stolz <donaldtstolz@gmail.com>
2025-06-23 21:05:35 -0500

    task: Nest pubsub setup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,16 +5,30 @@
 
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { GCPubSubServer } from 'nestjs-google-pubsub-microservice';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // App setup
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   const port = process.env.PORT || 3100;
+
+  // Microservice setup
+  app.connectMicroservice({
+    strategy: new GCPubSubServer({
+      topic: 'purchase-orders-topic',
+      subscription: 'purchase-orders-subscription',
+      client: {
+        projectId: 'goodday-exercise',
+      },
+    }),
+  });
+
+  await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
