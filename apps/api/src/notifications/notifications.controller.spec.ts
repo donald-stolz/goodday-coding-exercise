be26971a67ef5617943096489c1b784bfcdfbde4
Don Stolz <donaldtstolz@gmail.com>
2025-06-25 10:26:31 -0500

    task: Fix test cases

--- apps/api/src/notifications/notifications.controller.spec.ts
+++ apps/api/src/notifications/notifications.controller.spec.ts
@@ -1,14 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 describe('NotificationsController', () => {
   let controller: NotificationsController;
+  let notificationsServiceMock: DeepMockProxy<NotificationsService>;
 
   beforeEach(async () => {
+    notificationsServiceMock = mockDeep<NotificationsService>();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationsController],
-      providers: [NotificationsService],
+      providers: [
+        { provide: NotificationsService, useValue: notificationsServiceMock },
+      ],
     }).compile();
 
     controller = module.get<NotificationsController>(NotificationsController);
--- apps/api/src/notifications/notifications.service.spec.ts
+++ apps/api/src/notifications/notifications.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationsService } from './notifications.service';
+import { PurchaseOrdersService } from '../purchase-orders/purchase-orders.service';
+import { ItemsService } from '../items/items.service';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import nodemailer from 'nodemailer';
+
+jest.mock('nodemailer');
 
 describe('NotificationsService', () => {
   let service: NotificationsService;
+  let purchaseOrdersServiceMock: DeepMockProxy<PurchaseOrdersService>;
+  let itemsServiceMock: DeepMockProxy<ItemsService>;
 
   beforeEach(async () => {
+    purchaseOrdersServiceMock = mockDeep<PurchaseOrdersService>();
+    itemsServiceMock = mockDeep<ItemsService>();
+    (nodemailer.createTransport as jest.Mock).mockReturnValue({
+      sendMail: jest.fn(),
+    });
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificationsService],
+      providers: [
+        NotificationsService,
+        { provide: PurchaseOrdersService, useValue: purchaseOrdersServiceMock },
+        { provide: ItemsService, useValue: itemsServiceMock },
+      ],
     }).compile();
 
     service = module.get<NotificationsService>(NotificationsService);
